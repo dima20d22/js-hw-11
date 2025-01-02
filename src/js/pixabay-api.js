9de03914c105a48e322b4e3dc14f3411011cdeef
dima20d22 <dimasurovatka22@gmail.com>
2025-01-02 22:32:31 +0100

    + pictures

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,9 +3,12 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const input = document.querySelector("input[type=text]");
 const APIKey = "47994824-0d51099febe4e8602bb4fd3aa";
+const box = document.querySelector(".box");
 
 export function startSearch() {
   let q = input.value.trim();
+  console.log(q);
+
   if (q === "") {
     iziToast.show({
       message:
@@ -23,6 +26,7 @@ export function startSearch() {
       return response.json();
     })
     .then((data) => {
+      console.log(data.hits);
       const total = data.total;
       if (total === 0) {
         iziToast.show({
@@ -33,6 +37,24 @@ export function startSearch() {
         return;
       }
       console.log(data);
+
+      box.innerHTML = "";
+
+      data.hits.forEach((hit) => {
+        const li = document.createElement("li");
+        li.classList.add("li");
+        li.innerHTML = `
+          <img class="img" src="${hit.webformatURL}" alt="${hit.tags}" />
+          <div class = "div">
+          <p  class="p"><span class="p__span">Likes: </span> ${hit.likes}</p>
+          <p  class="p"><span class="p__span">Views: </span> ${hit.views}</p>
+          <p  class="p"><span class="p__span">Comments: </span> ${hit.comments}</p>
+          <p  class="p"><span class="p__span">Downloads: </span> ${hit.downloads}</p>
+        </div>
+        `;
+        box.appendChild(li);
+        console.log(box);
+      });
     })
     .catch((error) => {
       console.log(error);
